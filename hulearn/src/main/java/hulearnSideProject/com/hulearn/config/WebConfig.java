185376956c790bf91e5a5c6b32518de17aba4898
김재우 <rlawodnv2@naver.com>
2025-04-27 02:07:09 +0900

    프로젝트 내부 저장이라 따로 지정을 하지 않아도 괜찮지만 명시적으로 해놓음

--- hulearn/src/main/java/hulearnSideProject/com/hulearn/config/WebConfig.java
+++ hulearn/src/main/java/hulearnSideProject/com/hulearn/config/WebConfig.java
@@ -11,6 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
 		registry.addResourceHandler("/uploads/**")
-				.addResourceLocations("file:///C:/uploads/");
+				.addResourceLocations("classpath:/static/uploads/");
 	}
 }
