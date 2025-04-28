0bc1b20ba7545af1efb270a1a1f8780a01b4b864
KimJaeWoo <rlawodnv2@naver.com>
2025-04-28 12:51:37 +0900

    패키지 변경

---
+++ hulearn/src/main/java/heuron/com/dto/patient/PatientDto.java
@@ -0,0 +1,29 @@
+package heuron.com.dto.patient;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import heuron.com.dto.image.ImageDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PatientDto {
+	private long patiNo;
+	private String patiNm;
+	private Integer age;
+	private String genCd;
+	private Character diseaseYn;
+	private Character delYn;
+	private String hpNo;
+	private LocalDate regDts;
+	private LocalDate modDts;
+	private List<ImageDto> images;
+}
